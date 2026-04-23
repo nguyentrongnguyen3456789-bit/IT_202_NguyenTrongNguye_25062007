27e3f8b59ff8aae20442a223c2fc96fc5663d8aa
nguyentrongnguyen3456789-bit <nguyentrongnguyen3456789@gmail.com>
2026-04-23 14:02:17 +0700

    Update bai_03.sql

--- ss2/bai_03.sql
+++ ss2/bai_03.sql
@@ -1,8 +1,20 @@
 USE dtb_ss02;
 
+CREATE TABLE CUSTOMERS (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(100) NOT NULL
+);
+
 CREATE TABLE ORDERS (
-	order_id INT AUTO_INCREMENT PRIMARY KEY,
-    order_date DATE DEFAULT (CURRENT_DATE),
-    CustomerID INT NOT NULL,
-	CONSTRAINT FOREIGN KEY (CustomerID) REFERENCES CUSTOMERS(CustomerID)
+    order_id INT PRIMARY KEY AUTO_INCREMENT,         
+    order_date DATETIME DEFAULT CURRENT_TIMESTAMP,    
+    total_amount DECIMAL(10,2) NOT NULL,             
+    customer_id INT NOT NULL,                         
+
+    
+    CONSTRAINT fk_customer
+    FOREIGN KEY (customer_id)
+    REFERENCES CUSTOMERS(customer_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
