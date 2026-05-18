9d0e442c56738fc888959cc8a5b0f6256095f62a
nguyentrongnguyen3456789-bit <nguyentrongnguyen3456789@gmail.com>
2026-05-18 23:27:21 +0700

    Update bt2.sql

--- ss14/bt2.sql
+++ ss14/bt2.sql
@@ -22,12 +22,7 @@ DROP PROCEDURE IF EXISTS TransferBed;
 DELIMITER //
 
 CREATE PROCEDURE TransferBed(IN p_patient_id INT,IN p_new_bed_id INT)
-
 BEGIN
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION
-    BEGIN
-        ROLLBACK;
-    END;
     START TRANSACTION;
 		UPDATE Beds SET patient_id = NULL   WHERE patient_id = p_patient_id;
 		UPDATE Beds SET patient_id = p_patient_id    WHERE bed_id = p_new_bed_id;
